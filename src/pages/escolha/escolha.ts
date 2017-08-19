cdfbf7995c8938f586fbb3a45d8904b055cd2349
marcos pereira <marpernas@gmail.com>
2017-08-19 14:20:04 -0300

    adding acessorio in ecolha page

--- src/pages/escolha/escolha.ts
+++ src/pages/escolha/escolha.ts
@@ -5,14 +5,20 @@ import { NavParams } from 'ionic-angular';
 
 
 @Component({
-    templateUrl: 'escolha.html'
+  templateUrl: 'escolha.html'
 })
 export class EscolhaPage {
 
   public carro;
+  public acessorios;
 
   constructor(public navParams: NavParams) {
-    
+
     this.carro = navParams.get('carroSelecionado');
+    this.acessorios = [
+      { nome: 'Freio ABS', preco: 800 },
+      { nome: 'Ar-condicionado', preco: 1000 },
+      { nome: 'MP3 Player', preco: 500 }
+    ];
   }
 }
