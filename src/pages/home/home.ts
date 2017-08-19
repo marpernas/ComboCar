178faa19394d2cb2a6651f54ab5f13ed306b5356
marcos pereira <marpernas@gmail.com>
2017-08-19 11:41:13 -0300

    create project

---
+++ src/pages/home/home.ts
@@ -0,0 +1,60 @@
+import { Component,OnInit } from '@angular/core';
+
+// importanto novo componente
+import { NavController,LoadingController,AlertController } from 'ionic-angular';
+
+// importando a classe do serviço
+import {Http} from '@angular/http';
+
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+
+export class HomePage implements OnInit {
+
+  public carros;
+
+   constructor(
+    public navCtrl: NavController, 
+    private _http: Http,
+    private _loadingCTRL: LoadingController,
+    private _alertCTRL: AlertController) {}
+
+  ngOnInit() {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    
+    let loader = this._loadingCTRL.create({
+      content: "Buscando novos carros...Aguarde",
+    });
+    
+    loader.present(); // para exibir o loader que acabamos de construir
+
+    this._http
+      .get('https://aluracar.herokuapp.com')
+      .map(res => res.json())
+      .toPromise() 
+      .then(carros => {
+        this.carros = carros
+        loader.dismiss();
+      })
+
+      .catch(err =>{
+        // boa pratica para o erro
+        console.log(err);
+        loader.dismiss();
+
+        let alert = this._alertCTRL.create({
+          title: 'Falha na conexão',
+          buttons: [{text: 'Estou ciente'}],
+          subTitle: 'Não foi possivel obter a lista de carro. Tente mais tarde.'
+        }).present();
+      });
+  }
+  
+  seleciona(carro){
+    console.log(carro.nome);
+  }
+}
